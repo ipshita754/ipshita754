5dcddf20191efa7e858610d43f4918e5e892ae7a
ipshita754 <87749908+ipshita754@users.noreply.github.com>
2021-07-22 10:41:29 +0530

    Create inheitance.java

---
+++ inheitance.java
@@ -0,0 +1,22 @@
+class Planet{
+void round()
+System.out.println(move round);
+}
+
+class earth extends planet {
+void water()
+System.out.println(have water);
+}
+
+class mars extends planet{
+void red()
+System.out.println(red in color);
+}
+class inheritance{
+  public static void main(String[]args){
+  mars m1=new mars();
+  m1.round();
+  m2.water();
+  m3.red();
+}
+}
